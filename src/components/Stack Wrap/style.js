69c9203d3f67066c4bfced3fbbd16bb2f2e241a0
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-02-10 14:12:30 +0100

    stack section responsiveness

--- src/components/Stack Wrap/style.js
+++ src/components/Stack Wrap/style.js
@@ -2,8 +2,17 @@ import styled from 'styled-components';
 
 
 export const StackWrapStyle = styled.div`
-    display: flex;
-    width: 80%;
-    margin: 50px 10%;
+  display: flex;
+  width: 80%;
+  margin: 50px 10%;
 
+  @media (max-width: 790px) {
+    flex-wrap: wrap;
+  }
+
+  @media (max-width: 480px) {
+    flex-wrap: wrap;
+    width: 95%;
+    margin: 50px 2.5%;
+  }
 `;
--- src/components/Stack/style.js
+++ src/components/Stack/style.js
@@ -4,8 +4,23 @@ import styled from 'styled-components';
 export const StackStyle = styled.div`
   margin: 100px 0.8%;
   width: 15%;
+  
   img {
     width: 120px;
     height: 120px;
+  }
+
+  @media (max-width: 790px) {
+    width: 50%;
+    margin: 30px 0%;
+  }
+  img {
+    margin: auto 30%;
+  }
+
+  @media (max-width: 480px) {
+    img {
+      margin: auto 10%;
+    }
   }
 `;
