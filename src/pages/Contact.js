25138d3ea9d65218fb400c8a499804e10056ac7b
Theresa Obamwonyi <theresaobamwonyi@gmail.com>
2021-02-19 12:17:08 +0100

    under construction

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,6 +3,12 @@ import Skill from "../components/Skill";
 const Contact = () => {
   return (
     <div>
+      <h1
+        style={{ fontSize: "150px", textAlign: "center", paddingTop: "150px" }}
+      >
+        Under Construction
+      </h1>
+
       <Skill />
     </div>
   );
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -3,6 +3,7 @@ import Skill from "../components/Skill";
 const Experience = () => {
   return (
     <div>
+      <h1 style={{ fontSize: "150px", textAlign: "center", paddingTop: "150px" }}>Under Construction</h1>
       <Skill />
     </div>
   );
